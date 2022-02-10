22c2941ee3dbd60b214b4ad6dec61d25403e2226
yj0946 <yj123_wo@163.com>
2022-02-11 00:23:25 +0800

    2022-2-11 0:23 dev代码提交

--- src/main/java/com/fruitsalesplatform/log/LogAopAspect.java
+++ src/main/java/com/fruitsalesplatform/log/LogAopAspect.java
@@ -5,7 +5,6 @@ import com.fruitsalesplatform.annotation.LogAnno;
 import com.fruitsalesplatform.entity.Logtable;
 import com.fruitsalesplatform.entity.User;
 import com.fruitsalesplatform.service.LogtableService;
-import org.apache.struts2.ServletActionContext;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,9 +14,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.net.URLDecoder;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Aspect
@@ -62,11 +65,16 @@ public class LogAopAspect {
         try {
             // 整合了Struts，所有用这种方式获取session中属性(亲测有效)
             ServletRequestAttributes attr = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
-            HttpSession session = attr.getRequest().getSession(true);
-            Object strUserJson = session.getAttribute("userinfo");
+            HttpServletRequest request = attr.getRequest();
+            Map<String, Cookie> cookieMap = readCookieMap(request);
+            if(!cookieMap.containsKey("userinfo")){  //判断cookieMap是否含有该key
+                return null;
+            }
+            Object objUserJson = URLDecoder.decode(cookieMap.get("userinfo").getValue(), "utf-8");
+            String strJson = JSON.toJSONString(objUserJson);
+            objUserJson = JSON.parse(strJson);
 
-            //Object strUserJson = ServletActionContext.getRequest().getSession().getAttribute("userinfo");
-            User user = JSON.toJavaObject((JSON) JSON.toJSON(strUserJson), User.class);//获取session中的user对象进而获取操作人名字
+            User user = JSON.parseObject(objUserJson.toString(), User.class);//获取session中的user对象进而获取操作人名字
             logtable.setOperateor(user.getUserName());// 设置操作人
 
             //让代理方法执行
@@ -82,5 +90,21 @@ public class LogAopAspect {
             logtableService.addLog(logtable);// 添加日志记录
         }
         return result;
+    }
+
+    /**
+     * 将cookie封装成map
+     * @param request
+     * @return
+     */
+    private static Map<String, Cookie> readCookieMap(HttpServletRequest request){
+        Map<String, Cookie> cookieMap = new HashMap<>();
+        Cookie[] cookies = request.getCookies();        //获取所有的cookie值
+        if(cookies != null){
+            for (Cookie cookie : cookies){
+                cookieMap.put(cookie.getName(),cookie);
+            }
+        }
+        return cookieMap;
     }
 }
